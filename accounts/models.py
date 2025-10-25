419772ce821bdc11bb3ecbfa4a07ab6d16454577
Anil Chaudhary <chaudhary98.sc@gmail.com>
2025-10-26 05:38:44 +0545

    customer and vendor registration restriction and many more..

--- accounts/models.py
+++ accounts/models.py
@@ -39,7 +39,7 @@ class User(AbstractBaseUser):
     CUSTOMER=2
 
     ROLE_CHOICE=(
-        (VENDOR,'Restaurant'),
+        (VENDOR,'Vendor'),
         (CUSTOMER,'Customer'),
     )
     first_name=models.CharField(max_length=50)
@@ -72,6 +72,14 @@ class User(AbstractBaseUser):
     
     def has_module_perms(self,app_label):
         return True 
+    
+    def get_role(self):
+        if self.role==1:
+            user_role='Vendor'
+        elif self.role==2:
+            user_role='Customer'
+        return user_role
+    
     
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
